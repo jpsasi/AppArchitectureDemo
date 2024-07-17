08007df7bc4d446a8debff426e397a143abd7bc0
Sasikumar <jpsasi@gmail.com>
2024-07-17 12:31:28 +0530

    Initial Commit

---
+++ AppArchitectureDemo/AppArchitectureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppArchitectureDemoApp.swift
+//  AppArchitectureDemo
+//
+//  Created by Sasikumar JP on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AppArchitectureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppArchitectureDemoTests/AppArchitectureDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppArchitectureDemoTests.swift
+//  AppArchitectureDemoTests
+//
+//  Created by Sasikumar JP on 7/17/24.
+//
+
+import Testing
+@testable import AppArchitectureDemo
+
+struct AppArchitectureDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
